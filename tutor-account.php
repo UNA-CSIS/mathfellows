5e8072bf890f2b23769908050e70067fb41b0a0b
Holleigh Landers <hlanders@una.edu>
2019-03-21 15:18:34 -0500

    Added class

---
+++ tutor-account.php
@@ -0,0 +1,23 @@
+<?php
+	/*** Holleigh Landers ***/
+  include('tutor-header.php');
+?>
+  <body>
+    <div class="container">
+      
+      <div class="acct_msg">
+        Account settings:
+      </div>
+      
+      <div class="selection_box">
+        <div id="acct_url1">
+          <a href="#">Update email</a>
+        </div>
+        <div id="acct_url2">
+          <a href="#">Update password</a>
+        </div>
+      </div>
+      
+		  </div>
+	</body>
+</html>
---
+++ tutor-header.php
@@ -0,0 +1,44 @@
+<html>
+
+<!-- Holleigh Landers -->
+
+<!-- Signed in tutor header -->
+  
+  <head>
+    <meta charset="utf-8">
+    <title>Tutor</title>
+    <link rel="stylesheet" href="stylesheet.css">
+  
+  
+    <div class="tutor_header">
+      <div class="logo">
+        <a href="index.php">
+          <img src="UNA-logo.png">
+        </a>
+      </div>
+                
+      <a href="index.php">
+        <div class="website_name">
+          Math Fellows
+        </div>
+      </a>
+                
+      <div class="home_button">
+        <form action="index.php" method="post">
+          <button class="button" type="submit">Home</button>
+        </form>
+      </div>
+      
+      
+      <div class="account_and_sign_out_bttns">
+        <form id="sign_out_bttn" action="#" method="post">
+          <button class="button" type="submit">Sign out</button>
+        </form>
+        
+        <form id="account_bttn" action="tutor-account.php" method="post">
+          <button class="button" type="submit">Account</button>
+        </form>
+      </div>
+      
+    </div>
+  </head>
